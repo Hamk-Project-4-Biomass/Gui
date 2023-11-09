c09b2bf1e69d06dffd2b2ada0719d9358ce8c20d
Jean-Baptiste Van Parys <jeanbaptistevanparys@gmail.com>
2023-11-09 15:54:24 +0200

    war crimes

--- pyrealsense.py
+++ pyrealsense.py
@@ -3,27 +3,32 @@ import numpy as np
 import cv2
 from PIL import Image
 
+
 class RealSense:
 
     def __init__(self):
-        try:
-            self.pipe = rs.pipeline()
+        self.depth_pil_image = None
+        self.color_pil_image = None
+        self.color_image = None
+        self.depth_image = None
+        self.color_frame = None
+        self.depth_frame = None
+        self.frames = None
 
-            self.rs_config = rs.config()
+        self.pipe = rs.pipeline()
 
-            self.rs_config.enable_stream(rs.stream.depth, rs.format.z16, 30)
-            self.rs_config.enable_stream(rs.stream.color, rs.format.bgr8, 30)
+        self.rs_config = rs.config()
 
+        self.rs_config.enable_stream(rs.stream.depth, rs.format.z16, 30)
+        self.rs_config.enable_stream(rs.stream.color, rs.format.bgr8, 30)
 
+        self.pipe.start(self.rs_config)
 
-            self.pipe.start(self.rs_config)
+        self.sensor_rgb = self.pipe.get_active_profile().get_device().query_sensors()[1]
 
-            self.update()
-        except:
-            print("No device detected")
-        
+        self.sensor_depth = self.pipe.get_active_profile().get_device().query_sensors()[0]
 
-    
+        self.update()
 
     def update(self):
         self.frames = self.pipe.wait_for_frames()
@@ -37,7 +42,7 @@ class RealSense:
         # resize the color image to match depth image for display but keep aspect ratio
         scale = self.depth_frame.get_width() / self.color_frame.get_width()
         dim = (self.depth_frame.get_width(), int(self.color_frame.get_height() * scale))
-        self.color_image = cv2.resize(self.color_image, dim, interpolation = cv2.INTER_AREA)
+        self.color_image = cv2.resize(self.color_image, dim, interpolation=cv2.INTER_AREA)
 
         # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(self.depth_image, alpha=0.07), cv2.COLORMAP_MAGMA)
@@ -67,3 +72,102 @@ class RealSense:
 
     def pipe_start(self):
         self.pipe.start(self.rs_config)
+
+    def color_auto_exposure(self):
+        if self.sensor_rgb.get_option(rs.option.enable_auto_exposure):
+            self.sensor_rgb.set_option(rs.option.enable_auto_exposure, False)
+        else:
+            self.sensor_rgb.set_option(rs.option.enable_auto_exposure, True)
+
+    def set_color_exposure(self, value):
+        self.sensor_rgb.set_option(rs.option.exposure, value)
+
+    def set_color_gain(self, value):
+        self.sensor_rgb.set_option(rs.option.gain, value)
+
+    def set_color_brightness(self, value):
+        self.sensor_rgb.set_option(rs.option.brightness, value)
+
+    def set_color_contrast(self, value):
+        self.sensor_rgb.set_option(rs.option.contrast, value)
+
+    def set_color_hue(self, value):
+        self.sensor_rgb.set_option(rs.option.hue, value)
+
+    def set_color_saturation(self, value):
+        self.sensor_rgb.set_option(rs.option.saturation, value)
+
+    def set_color_sharpness(self, value):
+        self.sensor_rgb.set_option(rs.option.sharpness, value)
+
+    def set_color_gamma(self, value):
+        self.sensor_rgb.set_option(rs.option.gamma, value)
+
+    def set_color_white_balance(self, value):
+        self.sensor_rgb.set_option(rs.option.white_balance, value)
+
+    def set_color_auto_white_balance(self):
+        if self.sensor_rgb.get_option(rs.option.enable_auto_white_balance):
+            self.sensor_rgb.set_option(rs.option.enable_auto_white_balance, False)
+        else:
+            self.sensor_rgb.set_option(rs.option.enable_auto_white_balance, True)
+
+    def color_backlight_compensation(self):
+        if self.sensor_rgb.get_option(rs.option.backlight_compensation):
+            self.sensor_rgb.set_option(rs.option.backlight_compensation, False)
+        else:
+            self.sensor_rgb.set_option(rs.option.backlight_compensation, True)
+
+    def color_low_light_compensation(self):
+        if self.sensor_rgb.get_option(rs.option.low_light_compensation):
+            self.sensor_rgb.set_option(rs.option.low_light_compensation, False)
+        else:
+            self.sensor_rgb.set_option(rs.option.low_light_compensation, True)
+
+    def get_color_power_line_frequency(self):
+        return self.sensor_rgb.get_option(rs.option.power_line_frequency)
+
+    def get_color_auto_white_balance(self):
+        return self.sensor_rgb.get_option(rs.option.enable_auto_white_balance)
+
+    def get_color_auto_exposure(self):
+        return self.sensor_rgb.get_option(rs.option.enable_auto_exposure)
+
+    def get_color_exposure(self):
+        return self.sensor_rgb.get_option(rs.option.exposure)
+
+    def get_color_gain(self):
+        return self.sensor_rgb.get_option(rs.option.gain)
+
+    def get_color_brightness(self):
+        return self.sensor_rgb.get_option(rs.option.brightness)
+
+    def get_color_contrast(self):
+        return self.sensor_rgb.get_option(rs.option.contrast)
+
+    def get_color_hue(self):
+        return self.sensor_rgb.get_option(rs.option.hue)
+
+    def get_color_saturation(self):
+        return self.sensor_rgb.get_option(rs.option.saturation)
+
+    def get_color_sharpness(self):
+        return self.sensor_rgb.get_option(rs.option.sharpness)
+
+    def get_color_gamma(self):
+        return self.sensor_rgb.get_option(rs.option.gamma)
+
+    def get_color_white_balance(self):
+        return self.sensor_rgb.get_option(rs.option.white_balance)
+
+    def get_backlight_compensation(self):
+        return self.sensor_rgb.get_option(rs.option.backlight_compensation)
+
+    def get_low_light_compensation(self):
+        return self.sensor_rgb.get_option(rs.option.low_light_compensation)
+
+    def color_auto_white_balance(self):
+        self.sensor_rgb.set_option(rs.option.enable_auto_white_balance, True)
+
+    def set_color_power_line_frequency(self, value):
+        self.sensor_rgb.set_option(rs.option.power_line_frequency, value)
